747d7632362d832ff2c1ef3b25375d127dc14c24
Haseeb717 <haseeb.ahmad717@gmail.com>
2016-02-13 14:58:51 -0800

    make it better

--- db/migrate/20160113203800_add_email_to_food_expenditures.rb
+++ db/migrate/20160113203800_add_email_to_food_expenditures.rb
@@ -1,6 +1,6 @@
 class AddEmailToFoodExpenditures < ActiveRecord::Migration
   def change
-    add_column :food_expenditures, :edit_request, :booleann, :default => false
-    add_column :food_expenditures, :edit_able, :booleann, :default => false
+    add_column :food_expenditures, :edit_request, :boolean, :default => false
+    add_column :food_expenditures, :edit_able, :boolean, :default => false
   end
 end
