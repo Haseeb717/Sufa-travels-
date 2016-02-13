2dc7753420282098ec07fbe4861cc0d51ce7aed0
Haseeb717 <haseeb.ahmad717@gmail.com>
2016-02-13 15:00:55 -0800

    make it better

--- db/migrate/20160113203911_add_email_to_extra_expenditures.rb
+++ db/migrate/20160113203911_add_email_to_extra_expenditures.rb
@@ -1,6 +1,6 @@
 class AddEmailToExtraExpenditures < ActiveRecord::Migration
   def change
     add_column :extra_expenditures, :edit_request, :boolean, :default => false
-    add_column :extra_expenditures, :edit_able, :booleann, :default => false
+    add_column :extra_expenditures, :edit_able, :boolean, :default => false
   end
 end
